b693abc193888858be46ca56ed58db9030f24ab3
MKoth <lecorbeaulibre@gmail.com>
2017-11-29 11:29:19 +0200

    Fixed error

--- game.jsx
+++ game.jsx
@@ -5,8 +5,6 @@ import { Loop, Stage, Sprite, TileMap } from 'react-game-kit';
 class Game extends Component {
 	constructor(){
 		super();
-		this.getWrapperStyles = this.getWrapperStyles.bind(this);
-		
 	}
   	render() {
 		return (
